33f56746ac81c9682deacaf1654785129b8c2c91
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-25 12:40:54 +0100

    Basic Navbar setup.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using CirsaHackaton;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
