ceee0a403eece84523d58384cfe38ec0aab716fd
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-26 16:55:38 +0100

    Initialize dummy database.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<UserService>();
+
+//This Singleton is not static, it's an instance that will be injected across pages
+var userService = new UserService();
+userService.InitializeDummyDB();
+builder.Services.AddSingleton<UserService>(userService);
 
 await builder.Build().RunAsync();
